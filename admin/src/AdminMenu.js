59f7ff7b0241c1d00923da4adfa4175f1c1ec9db
JyotDhamelia07 <135057024+JyotDhamelia@users.noreply.github.com>
2024-03-06 08:27:55 +0530

    done last

--- admin/src/AdminMenu.js
+++ admin/src/AdminMenu.js
@@ -6,9 +6,9 @@ export default function AdminMenu() {
         <header class="text-gray-900 bg-indigo-300 body-font border-b border-opacity-20">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg>
+                    <div>
+                        <img src="./images/logo.png" height="70" width="70" />
+                    </div>
                     <span class="ml-3 text-xl font-bold">Shop-Swift<span className="text-sm font-semibold"> (Admin Pannel)</span></span>
                 </a>
                 <nav class="md:ml-auto flex flex-wrap items-center text-base text-gray-900 justify-center">
