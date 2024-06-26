f8122af767ed400463d24d517b00647b5dc7efcb
Jyot Dhamelia <135057024+JyotDhamelia@users.noreply.github.com>
2024-06-26 18:00:56 +0530

    ConvertedToFunctionalComponent

--- admin/src/AdminUsers.js
+++ admin/src/AdminUsers.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminMenu from "./AdminMenu";
 import Footer from "./Footer";
 import axios from "axios";
 import BaseAddress from "./BaseAddress";
-import { IsLogedIn2 } from './ClassCookies';
-import { withCookies } from "react-cookie";
 import showError, { showMessage } from "./toast-message";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-class AdminUsers extends IsLogedIn2 {
+function AdminUsers() {
 
-    DisplayUsers = function (item) {
+    let [users, setUsers] = useState([]);
+
+    let DisplayUsers = function (item) {
         return (<>
             <tr class="bg-indigo-100 border-b dark:bg-indigo-100 dark:border-gray-700 hover:bg-indigo-50 dark:hover:bg-indigo-600">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -30,14 +30,7 @@ class AdminUsers extends IsLogedIn2 {
         </>);
     }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: []
-        }
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         let Apiaddress = BaseAddress() + "users.php";
         axios({
             method: 'get',
@@ -55,46 +48,45 @@ class AdminUsers extends IsLogedIn2 {
                 }
                 else {
                     data.splice(0, 2);
-                    this.setState({
-                        users: data
-                    })
+                    setUsers(data);
                 }
             }
-        })
-    }
+        }).catch((error) => {
+            showError('oops something went wrong, please contact developer....');
+        });
+    });
 
-    render() {
-        return (<>
-            <AdminMenu />
-            <ToastContainer />
-            <div className="mx-4 md:mx-16 mt-7">
-                <h1 className="mt-7 font-bold text-xl text-gray-700">Manage Users</h1>
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-7">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 text-center capitalize">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3" width="10%">
-                                    User Id
-                                </th>
-                                <th scope="col" class="px-6 py-3" width="10%">
-                                    Email
-                                </th>
-                                <th scope="col" class="px-6 py-3" width="5%">
-                                    Mobile
-                                </th>
-                                <th scope="col" class="px-6 py-3" width="10%">
-                                    View Details
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.users.map((item) => this.DisplayUsers(item))}
-                        </tbody>
-                    </table>
-                </div>
+    return (<>
+        <AdminMenu />
+        <ToastContainer />
+        <div className="mx-4 md:mx-16 mt-7">
+            <h1 className="mt-7 font-bold text-xl text-gray-700">Manage Users</h1>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-7">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 text-center capitalize">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3" width="10%">
+                                User Id
+                            </th>
+                            <th scope="col" class="px-6 py-3" width="10%">
+                                Email
+                            </th>
+                            <th scope="col" class="px-6 py-3" width="5%">
+                                Mobile
+                            </th>
+                            <th scope="col" class="px-6 py-3" width="10%">
+                                View Details
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((item) => DisplayUsers(item))}
+                    </tbody>
+                </table>
             </div>
-            <Footer />
-        </>);
-    }
+        </div>
+        <Footer />
+    </>);
 }
-export default withCookies(AdminUsers);
+
+export default AdminUsers;
