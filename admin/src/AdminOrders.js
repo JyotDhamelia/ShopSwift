cea1fbf9a6d89bab49143f58cb1c33d512787f9a
Jyot Dhamelia <135057024+JyotDhamelia@users.noreply.github.com>
2024-06-26 17:51:24 +0530

    ConvertedToFunctionalComponent

--- admin/src/AdminOrders.js
+++ admin/src/AdminOrders.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminMenu from "./AdminMenu";
 import Footer from "./Footer";
 import BaseAddress from "./BaseAddress";
 import axios from "axios";
-import { IsLogedIn2 } from './ClassCookies';
-import { withCookies } from "react-cookie";
 import showError, { showMessage } from "./toast-message";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-class AdminOrders extends IsLogedIn2 {
+import { Link } from "react-router-dom";
+import IsLogedIn from "./FunctionalCookies";
 
-    DisplayOrders = function (item) {
+function AdminOrders() {
+
+    IsLogedIn();
+
+    let [orders, setOrders] = useState([]);
+
+    let DisplayOrders = function (item) {
         return (<>
             <tr class="bg-indigo-100 border-b dark:bg-indigo-100 dark:border-gray-700 hover:bg-indigo-50 dark:hover:bg-indigo-600">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -26,26 +31,19 @@ class AdminOrders extends IsLogedIn2 {
                     {item.billdate}
                 </td>
                 <td class="px-6 py-4">
-                    {item.amount}
+                    ₹{item.amount}
                 </td>
                 <td class="px-6 py-4">
                     {item.orderstatus}
                 </td>
                 <td class="px-6 py-4">
-                    <a href="" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                    <Link to={"/admin-view-orders/" + item.id} class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</Link>
                 </td>
             </tr>
         </>)
     }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            orders: []
-        }
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         let Apiaddress = BaseAddress() + "orders.php";
         axios({
             method: 'get',
@@ -63,53 +61,51 @@ class AdminOrders extends IsLogedIn2 {
                 }
                 else {
                     data.splice(0, 2);
-                    this.setState({
-                        orders: data
-                    });
+                    setOrders(data);
                 }
             }
+        }).catch((error) => {
+            showError('oops something went wrong, please contact developer....');
         });
-    }
+    });
 
-    render() {
-        return (<>
-            <AdminMenu />
-            <ToastContainer/>
-            <div className="mx-4 md:mx-16 mt-7">
-                <h1 className="mt-7 font-bold text-xl text-gray-700">Manage Orders</h1>
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-7">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 text-center capitalize">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3" width="10%">
-                                    Order Id
-                                </th>
-                                <th scope="col" class="px-6 py-3" width="10%">
-                                    Customer Detail
-                                </th>
-                                <th scope="col" class="px-6 py-3" width="5%">
-                                    Date
-                                </th>
-                                <th scope="col" class="px-6 py-3" width="10%">
-                                    Ammount
-                                </th>
-                                <th scope="col" class="px-6 py-3" width="10%">
-                                    Status
-                                </th>
-                                <th scope="col" class="px-6 py-3" width="10%">
-                                    View Details
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.orders.map((item) => this.DisplayOrders(item))}
-                        </tbody>
-                    </table>
-                </div>
+    return (<>
+        <AdminMenu />
+        <ToastContainer />
+        <div className="mx-4 md:mx-16 mt-7">
+            <h1 className="mt-7 font-bold text-xl text-gray-700">Manage Orders</h1>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-7">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 text-center capitalize">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3" width="10%">
+                                Order Id
+                            </th>
+                            <th scope="col" class="px-6 py-3" width="10%">
+                                Customer Detail
+                            </th>
+                            <th scope="col" class="px-6 py-3" width="5%">
+                                Date
+                            </th>
+                            <th scope="col" class="px-6 py-3" width="10%">
+                                Ammount
+                            </th>
+                            <th scope="col" class="px-6 py-3" width="10%">
+                                Status
+                            </th>
+                            <th scope="col" class="px-6 py-3" width="10%">
+                                View Details
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((item) => DisplayOrders(item))}
+                    </tbody>
+                </table>
             </div>
-            <Footer />
-        </>);
-    }
+        </div>
+        <Footer />
+    </>);
 }
 
-export default withCookies(AdminOrders);
+export default AdminOrders;
