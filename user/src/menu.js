57894bd947e24893385dc4a22a69745e480eaa5c
Jyot Dhamelia <135057024+JyotDhamelia@users.noreply.github.com>
2024-12-10 23:27:37 +0530

    menu added

--- user/src/menu.js
+++ user/src/menu.js
@@ -1,89 +1,88 @@
 import { useCookies } from "react-cookie";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
-  let [cookies, setCookie] = useCookies(["user"]);
-  let output = null;
+  const [cookies] = useCookies(["user"]);
   const isLoggedIn = cookies['userid'] !== undefined;
-  return (
-    <header className="navbar d-block navbar-sticky navbar-expand-lg navbar-light bg-light">
-      <div className="container pb-4">
-        <a
-          className="navbar-brand d-none d-sm-block me-4 order-lg-1"
-          href="index.html"
-        >
-          <h1>Online Shop</h1>
-        </a>
-        <a
-          className="navbar-brand d-sm-none me-2 order-lg-1"
-          href="index.html"
-        >
-          <h1>Online Shop</h1>
-        </a>
 
-        <div
-          className="collapse navbar-collapse me-auto order-lg-2"
-          id="navbarCollapse"
+  return (
+    <header
+      className="navbar navbar-expand-lg text-dark shadow-sm"
+      style={{ backgroundColor: "#B3C7F9" }} // Light blue color from the image
+    >
+      <div className="container">
+        {/* Brand with image */}
+        <Link className="navbar-brand d-flex align-items-center text-dark" to="/">
+          <img
+            src="./images/Shop-Swift.jpg"
+            alt=""
+            style={{ width: "50px", height: "50px" }}
+            className="me-2"
+          />
+          <span className="fw-bold fs-5">Shop-Swift</span>
+        </Link>
+
+        {/* Toggle button for smaller screens */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          {/* Search (mobile)*/}
-          <div className="d-lg-none py-3">
-            <div className="input-group">
-              <i className="ci-search position-absolute top-50 start-0 translate-middle-y ms-3" />
-              <input
-                className="form-control rounded-start"
-                type="text"
-                placeholder="What do you need?"
-              />
-            </div>
-          </div>
-          {/* Primary menu*/}
-          <ul className="navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Items */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link text-dark fw-bold" to="/">
                 Home
-              </a>
+              </Link>
             </li>
-            { isLoggedIn === true ? (
+            {isLoggedIn ? (
               <>
                 <li className="nav-item">
-                  <a className="nav-link" href="/cart">
+                  <Link className="nav-link text-dark fw-bold" to="/cart">
                     Cart
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/wishlist">
+                  <Link className="nav-link text-dark fw-bold" to="/wishlist">
                     Wishlist
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/checkout">
+                  <Link className="nav-link text-dark fw-bold" to="/checkout">
                     Checkout
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/logout">
+                  <Link className="nav-link text-dark fw-bold" to="/logout">
                     Logout
-                  </a>
+                  </Link>
                 </li>
               </>
             ) : (
               <>
                 <li className="nav-item">
-                  <a className="nav-link" href="/register">
+                  <Link className="nav-link text-dark fw-bold" to="/register">
                     Register
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/login">
+                  <Link className="nav-link text-dark fw-bold" to="/login">
                     Login
-                  </a>
+                  </Link>
                 </li>
               </>
             )}
-
           </ul>
         </div>
       </div>
-
     </header>
-  )
+  );
 }
